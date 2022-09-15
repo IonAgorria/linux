e04dfd555895c06624c138361c1da8f0c21cfffe
Svyatoslav Ryhel <clamor95@gmail.com>
2022-09-15 17:57:00 +0300

    max77663-rtc: pass rtc address from device tree node if exists
    MAX77663 PMIC can have RTC on both 0x63 i2c address (like grouper)
    which is main address but on some devices RTC is located on 0x48
    i2c address (like p880 and p895 from LG). Lets add property to be
    able to use alternative address if needed without breaking existing
    bindings.
    
    Signed-off-by: Svyatoslav Ryhel <clamor95@gmail.com>

--- drivers/rtc/rtc-max77686.c
+++ drivers/rtc/rtc-max77686.c
@@ -691,6 +691,7 @@ static int max77686_init_rtc_regmap(struct max77686_rtc_info *info)
 	struct device *parent = info->dev->parent;
 	struct i2c_client *parent_i2c = to_i2c_client(parent);
 	struct i2c_client *client;
+	int rtc_i2c_addr = info->drv_data->rtc_i2c_addr;
 	int ret;
 
 	if (info->drv_data->rtc_irq_from_platform) {
@@ -713,8 +714,10 @@ static int max77686_init_rtc_regmap(struct max77686_rtc_info *info)
 		goto add_rtc_irq;
 	}
 
+	device_property_read_u32(parent, "maxim,rtc-i2c-address", &rtc_i2c_addr);
+
 	client = devm_i2c_new_dummy_device(info->dev, parent_i2c->adapter,
-					   info->drv_data->rtc_i2c_addr);
+					   rtc_i2c_addr);
 	if (IS_ERR(client))
 		return dev_err_probe(info->dev, PTR_ERR(client),
 				     "Failed to allocate I2C device for RTC\n");
