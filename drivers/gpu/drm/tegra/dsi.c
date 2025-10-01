270388960ffaf3a2b448a3cccb71c0688a686d4c
Svyatoslav Ryhel <clamor95@gmail.com>
2025-10-01 17:25:39 +0300

    Revert "[TEMP] gpu/drm: tegra: dsi: add support for Tegra20/Tegra30"
    This reverts commit 23dc40540244db30bfe532877a011edef98340a6.

--- drivers/gpu/drm/tegra/dsi.c
+++ drivers/gpu/drm/tegra/dsi.c
@@ -662,47 +662,38 @@ static int tegra_dsi_pad_enable(struct tegra_dsi *dsi)
 {
 	u32 value;
 
-	/* Tegra20/30 uses DSIv0 while Tegra114+ uses DSIv1 */
-	if (of_device_is_compatible(dsi->dev->of_node, "nvidia,tegra20-dsi") ||
-	    of_device_is_compatible(dsi->dev->of_node, "nvidia,tegra30-dsi")) {
-		value = DSI_PAD_CONTROL_LPUPADJ(0x1) | DSI_PAD_CONTROL_LPDNADJ(0x1) |
-			DSI_PAD_CONTROL_PREEMP_EN(0x1) | DSI_PAD_CONTROL_SLEWDNADJ(0x6) |
-			DSI_PAD_CONTROL_SLEWUPADJ(0x6) | DSI_PAD_CONTROL_PDIO(0) |
-			DSI_PAD_CONTROL_PDIO_CLK(0) | DSI_PAD_CONTROL_PULLDN_ENAB(0);
-		tegra_dsi_writel(dsi, value, DSI_PAD_CONTROL_0);
-	} else {
-		/*
-		 * XXX Is this still needed? The module reset is deasserted right
-		 * before this function is called.
-		 */
-		tegra_dsi_writel(dsi, 0, DSI_PAD_CONTROL_0);
-		tegra_dsi_writel(dsi, 0, DSI_PAD_CONTROL_1);
-		tegra_dsi_writel(dsi, 0, DSI_PAD_CONTROL_2);
-		tegra_dsi_writel(dsi, 0, DSI_PAD_CONTROL_3);
-		tegra_dsi_writel(dsi, 0, DSI_PAD_CONTROL_4);
-
-		value = DSI_PAD_CONTROL_VS1_PULLDN(0) | DSI_PAD_CONTROL_VS1_PDIO(0);
-		tegra_dsi_writel(dsi, value, DSI_PAD_CONTROL_0);
-
-		value = DSI_PAD_SLEW_UP(0x7) | DSI_PAD_SLEW_DN(0x7) |
-			DSI_PAD_LP_UP(0x1) | DSI_PAD_LP_DN(0x1) |
-			DSI_PAD_OUT_CLK(0x0);
-		tegra_dsi_writel(dsi, value, DSI_PAD_CONTROL_2);
-
-		value = DSI_PAD_PREEMP_PD_CLK(0x3) | DSI_PAD_PREEMP_PU_CLK(0x3) |
-			DSI_PAD_PREEMP_PD(0x03) | DSI_PAD_PREEMP_PU(0x3);
-		tegra_dsi_writel(dsi, value, DSI_PAD_CONTROL_3);
-	}
+	value = DSI_PAD_CONTROL_VS1_PULLDN(0) | DSI_PAD_CONTROL_VS1_PDIO(0);
+	tegra_dsi_writel(dsi, value, DSI_PAD_CONTROL_0);
 
 	return 0;
 }
 
 static int tegra_dsi_pad_calibrate(struct tegra_dsi *dsi)
 {
+	u32 value;
 	int err;
+
+	/*
+	 * XXX Is this still needed? The module reset is deasserted right
+	 * before this function is called.
+	 */
+	tegra_dsi_writel(dsi, 0, DSI_PAD_CONTROL_0);
+	tegra_dsi_writel(dsi, 0, DSI_PAD_CONTROL_1);
+	tegra_dsi_writel(dsi, 0, DSI_PAD_CONTROL_2);
+	tegra_dsi_writel(dsi, 0, DSI_PAD_CONTROL_3);
+	tegra_dsi_writel(dsi, 0, DSI_PAD_CONTROL_4);
 
 	/* start calibration */
 	tegra_dsi_pad_enable(dsi);
+
+	value = DSI_PAD_SLEW_UP(0x7) | DSI_PAD_SLEW_DN(0x7) |
+		DSI_PAD_LP_UP(0x1) | DSI_PAD_LP_DN(0x1) |
+		DSI_PAD_OUT_CLK(0x0);
+	tegra_dsi_writel(dsi, value, DSI_PAD_CONTROL_2);
+
+	value = DSI_PAD_PREEMP_PD_CLK(0x3) | DSI_PAD_PREEMP_PU_CLK(0x3) |
+		DSI_PAD_PREEMP_PD(0x03) | DSI_PAD_PREEMP_PU(0x3);
+	tegra_dsi_writel(dsi, value, DSI_PAD_CONTROL_3);
 
 	err = tegra_mipi_start_calibration(dsi->mipi);
 	if (err < 0)
@@ -1614,7 +1605,7 @@ static int tegra_dsi_probe(struct platform_device *pdev)
 		goto remove;
 	}
 
-	dsi->clk_lp = devm_clk_get_optional(&pdev->dev, "lp");
+	dsi->clk_lp = devm_clk_get(&pdev->dev, "lp");
 	if (IS_ERR(dsi->clk_lp)) {
 		err = dev_err_probe(&pdev->dev, PTR_ERR(dsi->clk_lp),
 				    "cannot get low-power clock\n");
@@ -1635,14 +1626,10 @@ static int tegra_dsi_probe(struct platform_device *pdev)
 		goto remove;
 	}
 
-	/* Tegra20/Tegra30 do not use DSI parent muxing */
-	if (!of_device_is_compatible(dsi->dev->of_node, "nvidia,tegra20-dsi") &&
-	    !of_device_is_compatible(dsi->dev->of_node, "nvidia,tegra30-dsi")) {
-		err = tegra_dsi_setup_clocks(dsi);
-		if (err < 0) {
-			dev_err(&pdev->dev, "cannot setup clocks\n");
-			return err;
-		}
+	err = tegra_dsi_setup_clocks(dsi);
+	if (err < 0) {
+		dev_err(&pdev->dev, "cannot setup clocks\n");
+		goto remove;
 	}
 
 	dsi->regs = devm_platform_ioremap_resource(pdev, 0);
@@ -1711,8 +1698,6 @@ static const struct of_device_id tegra_dsi_of_match[] = {
 	{ .compatible = "nvidia,tegra132-dsi", },
 	{ .compatible = "nvidia,tegra124-dsi", },
 	{ .compatible = "nvidia,tegra114-dsi", },
-	{ .compatible = "nvidia,tegra30-dsi", },
-	{ .compatible = "nvidia,tegra20-dsi", },
 	{ },
 };
 MODULE_DEVICE_TABLE(of, tegra_dsi_of_match);
